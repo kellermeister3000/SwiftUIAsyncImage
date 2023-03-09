cb0c29f1ad8592cb026704148ca7330452541a5b
Philip Keller <philipjoelkeller@gmail.com>
2023-03-09 12:38:43 -0800

    Initial Commit

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Philip Keller on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
